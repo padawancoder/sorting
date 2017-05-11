a3a7a6e4ff28e956e7357cd011669bb940897184
Cooper <padawan@amazon.com>
2017-05-10 22:21:39 -0700

    Added quicksort, need to write test next.

---
+++ src/com/padawan/sort/QuickSort.java
@@ -0,0 +1,48 @@
+package com.padawan.sort;
+
+import java.util.List;
+
+
+public class QuickSort implements Sort<Integer> {
+
+    public void sort(Integer[] array) {
+        quickSort(array, 0, array.length);
+    }
+
+    private void quickSort(Integer[] array, int low, int high){
+        if(array == null || array.length == 0) return;
+
+        if(low >= high) return;
+
+        int middle = (low + (high-low))/2;
+        Integer pivot = array[middle];
+
+        int i = low, j = high;
+
+        while (i <= j){
+            while(array[i] < pivot){
+                i++;
+            }
+            while(array[j] > pivot){
+                j--;
+            }
+
+            if(i<=j){
+                Integer temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        if(low < j){
+            quickSort(array, low, j);
+        }
+        if(high > i){
+            quickSort(array, i, high);
+        }
+
+    }
+
+}
---
+++ src/com/padawan/sort/Sort.java
@@ -0,0 +1,8 @@
+package com.padawan.sort;
+
+
+import java.util.List;
+
+public interface Sort<T> {
+    void sort(T[] array);
+}
